38a11c2272694525c12c4f36074cf8dd8a24db0f
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-18 12:57:33 +0300

    Refactor App component to add cart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import { Button } from "./components/button/Button";
 import { Card } from "./components/card/Card";
@@ -6,15 +7,44 @@ import { getData } from "./db/db";
 const foods = getData();
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const onAdd = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...food, quantity: 1 }]);
+    }
+  };
+
+  const onRemove = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist.quantity === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== food.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+        )
+      );
+    }
+  };
+
   return (
     <>
-      <Button title={"Test"} disabled={false} type={"add"} />
-      <Button title={"Test"} disabled={false} type={"remove"} />
-      <Button title={"Test"} disabled={false} type={"checkout"} />
+      <h1 className="heading">Order Food</h1>
+      <Cart cartItems={cartItems} onCheckout={onCheckout} />
       <div className="cards__container">
-        {foods.map((food) => (
-          <Card key={food.id} food={food} />
-        ))}
+        {foods.map((food) => {
+          return (
+            <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
+          );
+        })}
       </div>
     </>
   );
