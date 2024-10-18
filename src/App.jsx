c89d4846b82190e54a0e37c0fdceed07c035305e
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-18 12:36:50 +0300

    Refactor CSS imports and remove unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import { Button } from './components/button/Button'
 
 function App() {
 
   return (
     <>
+    <Button title={'Test'} disabled={false} type={'add'} />
+    <Button title={'Test'} disabled={false} type={'remove'} />
+    <Button title={'Test'} disabled={false} type={'checkout'} />
     </>
   )
 }
